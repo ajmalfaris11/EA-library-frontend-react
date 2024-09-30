69b2bda06d4a958b99669abf67ddb55080504ba1
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-09-30 19:20:44 +0530

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Books from './pages/Books';
+import Authors from './pages/Authors';
+import Book from './pages/Book';
+import Author from './pages/Author';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/books/:id" element={<Book />} />
+        <Route path="/authors" element={<Authors />} />
+        <Route path="/authors/:id" element={<Author />} />
+      </Routes>
+    </Router>
   );
 }
 
