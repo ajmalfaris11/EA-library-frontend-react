81522eaa2a293097ebadb95526ef8fe2bdc120c4
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-10-02 00:19:10 +0530

    Single Author Page Styled

--- src/pages/Author.js
+++ src/pages/Author.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { Container, Typography, Card, CardContent, Box } from '@mui/material';
 
 const Author = () => {
   const { id } = useParams();
@@ -14,9 +15,18 @@ const Author = () => {
   if (!author) return <div>Loading...</div>;
 
   return (
-    <div>
-      <h1>{author.name}</h1>
-    </div>
+    <Container sx={{ marginTop: 4 }}>
+      <Card sx={{ backgroundColor: '#f5f5f5', padding: 2 }}>
+        <CardContent>
+          <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
+            {author.name}
+          </Typography>
+          <Typography variant="body1" sx={{ marginTop: 2 }}>
+            {author.biography}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Container>
   );
 };
 
