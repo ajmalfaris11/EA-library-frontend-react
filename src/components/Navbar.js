3a27c3a299b294e6296209b2412d9a39916d5170
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-10-01 22:06:25 +0530

    Navbar Styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/books">Books</Link></li>
-        <li><Link to="/authors">Authors</Link></li>
-      </ul>
-    </nav>
+    <AppBar position="static" sx={{ backgroundColor: '#1976d2' }}>
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1, fontWeight: 'bold' }}>
+          Premium Library
+        </Typography>
+        <Button color="inherit" component={Link} to="/">Home</Button>
+        <Button color="inherit" component={Link} to="/books">Books</Button>
+        <Button color="inherit" component={Link} to="/authors">Authors</Button>
+      </Toolbar>
+    </AppBar>
   );
 };
 
