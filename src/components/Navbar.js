1fb5c113d2a9de5c81d5bf43388d8319be1ba63e
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-10-01 22:30:48 +0530

    home page designed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#1976d2' }}>
+    <AppBar position="static" sx={{ backgroundColor: '#333333' }}>
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1, fontWeight: 'bold' }}>
-          Premium Library
+        Literary Haven
         </Typography>
         <Button color="inherit" component={Link} to="/">Home</Button>
         <Button color="inherit" component={Link} to="/books">Books</Button>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Container, Typography, Box } from '@mui/material';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Library</h1>
-    </div>
+    <Box
+      sx={{
+        backgroundImage: 'url("https://png.pngtree.com/background/20230426/original/pngtree-an-old-library-has-wooden-step-leading-to-stairs-picture-image_2486870.jpg")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Container>
+        <Typography
+          variant="h2"
+          align="center"
+          sx={{ color: '#fff', fontWeight: 'bold', textShadow: '2px 2px 4px rgba(0,0,0,0.7)' }}
+        >
+          Welcome to The Literary Haven
+        </Typography>
+        <Typography
+          variant="h5"
+          align="center"
+          sx={{ color: '#fff', marginTop: 2, textShadow: '2px 2px 4px rgba(0,0,0,0.7)' }}
+        >
+          Explore a world of knowledge
+        </Typography>
+      </Container>
+    </Box>
   );
 };
 
