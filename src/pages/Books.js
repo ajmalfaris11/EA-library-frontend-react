8c23cfbb26bff3d77720a5ebc30ef7500ae8a832
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-10-02 23:51:20 +0530

    books page some issues fied

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Typography, Grid, Card, CardContent, CardMedia } from '@mui/material';
+import { Container, Typography, Grid2, Card, CardContent, CardMedia } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const Books = () => {
@@ -16,9 +16,9 @@ const Books = () => {
       <Typography variant="h2" align="center" gutterBottom sx={{ fontWeight: 'bold' }}>
         Books Collection
       </Typography>
-      <Grid container spacing={4}>
+      <Grid2 container spacing={4}>
         {books.map(book => (
-          <Grid item xs={12} sm={6} md={4} key={book._id}>
+          <Grid2 item xs={12} sm={6} md={4} key={book._id}>
             <Card
               sx={{
                 backgroundColor: '#f5f5f5',
@@ -43,9 +43,9 @@ const Books = () => {
                 <Typography color="textSecondary">Author: {book.authorName}</Typography>
               </CardContent>
             </Card>
-          </Grid>
+          </Grid2>
         ))}
-      </Grid>
+      </Grid2>
     </Container>
   );
 };
